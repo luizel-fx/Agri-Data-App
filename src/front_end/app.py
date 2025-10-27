742bd95c42ab4bd6ca3c7e675a26f36eb4974f36
Luiz E. Lima <luizeduardodelima4@gmail.com>
2025-10-27 15:37:47 -0300

    navbar finished

--- src/front_end/app.py
+++ src/front_end/app.py
@@ -2,146 +2,16 @@ import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
+from components.navbar import navbar
 
 # SETTING THEME AND COSMETICS
 THEME = [dbc.themes.LUX]
 app = dash.Dash(__name__, external_stylesheets=THEME)
 app.title = "Agrimensor"
 
-# NAVIGATION BAR
-def navItem(label, href):
-    return dbc.NavLink(label, href=href, active="exact")
-
 LOGO = "/assets/logo.png"
 
-def nav_item(label, href):
-    """Cria um item de navegação com link para o sidebar."""
-    nav_item_style={
-        'font-size': 20,
-        'display': 'flex',
-        'align-items': 'center',
-        'font-weight': 'normal',
-        'margin-left': 0,
-        'margin-right': 0,
-        'color': '#383838'
-        }
-    return dbc.NavItem(dbc.NavLink(label, href=href, active="exact"), style = nav_item_style)
-
-def drop_down_menu(label, items_url: dict, icon_path):
-    # Estilos para o botão (toggle) do DropdownMenu
-    # Removendo 'display: flex' e 'align-items: center' daqui, pois
-    # isso será aplicado ao conteúdo do 'label' (o span)
-    drop_down_menu_style = {
-        "background": "#FFFFFF",
-        "color": "#383838",
-        "font-size": 20,
-        'font-weight': 'normal',
-        "padding-right": "0.5rem",
-        "display": "flex",
-        "align-items": "center",
-    }
-
-    # 1. Cria o elemento do ícone
-    icon_img = html.Img(
-        src=icon_path, 
-        width='20px', 
-        height='20px', 
-        style={'margin-right': '5px'} # Adiciona um pequeno espaço à direita do ícone
-    )
-
-    # 2. Combina o ícone e o rótulo em um html.Span.
-    # Usamos 'display: flex' aqui para alinhar horizontalmente o ícone e o texto
-    dropdown_label_content = html.Span(
-        [icon_img, label],
-        style={
-            'display': 'flex', 
-            'align-items': 'center', 
-            'line-height': 'normal' # Ajuda na centralização vertical
-        } 
-    )
-
-    # Cria os itens do menu suspenso (o que cai)
-    children = [
-        dbc.DropdownMenuItem(
-            # Assumindo que 'nav_item' retorna um componente de link (como um 'dbc.NavLink')
-            nav_item(item, items_url[item]), 
-            class_name='ms-2'
-        ) for item in items_url.keys()
-    ]
-
-    return dbc.DropdownMenu(
-        # Passa o Span (que contém o ícone e o texto) como o rótulo
-        label=dropdown_label_content,
-        children=children,
-        toggle_style=drop_down_menu_style,
-        # Você pode querer adicionar um 'className' para estilizar o botão com mais precisão via CSS externo
-    )
-
-reports_pages = {
-    'Condições das lavouras': '/',
-    'Progresso da safra': '/',
-    'Oferta e Demanda - USDA': '/',
-    'Exportações (EUA)': '/'
-}
-
-spot_pages = {
-    "Basis": "pages/mercado_fisico/basis",
-    "Diferencial de base": "pages/mercado_fisico/basis_diff",
-    "Preços relativo": "pages/mercado_fisico/rel_prices"
-}
-
-fut_pages = {
-    "Spreads": "/",
-    "Ratios": "/"
-}
-
-home_navbar = html.Div(children=[
-    html.Img(src="/assets/home_icon.png", width='20px', height='20px'),
-    nav_item("Home", "/")
-], style={'display': 'flex', 'align-items': 'center','margin-left': 0, 'margin-right': 0,})
-
-navbar = dbc.Navbar(
-    dbc.Container(
-        [
-            # LOGO + 
-            html.A(
-                dbc.Row(
-                    children=[
-                        html.Div(
-                            [
-                                # LOGO
-                                html.Img(src=LOGO, height='100px', className='me-2'),
-                                #dbc.NavbarBrand("Agrimensor", className='ms-2')
-
-                                # NAVBAR
-                                html.Div(
-                                    [
-                                        #nav_item("Home", "/"),
-                                        home_navbar,
-                                        drop_down_menu("Relatórios", reports_pages, 'assets/reports_icon.png'),
-                                        drop_down_menu("Mercado físico", spot_pages, 'assets/spot_icon.png'),
-                                        drop_down_menu("Mercado futuro", fut_pages, 'assets/fut_icon.png')
-                                    ],
-                                    style={"display": 'flex'}
-                                )
-                            ],
-                            style={
-                                "display": 'flex',
-                                "align-items": "center",
-                                "justify-content": "center"
-                            }
-                        )
-                        ]
-                    ),
-                )
-            ]
-        )
-    )
-
-
-
-
-app.layout = html.Div(children=[navbar])
+app.layout = html.Div(children=[navbar()])
 
 
 app.run()
---
+++ src/front_end/components/navbar.py
@@ -0,0 +1,133 @@
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output, State
+import dash_bootstrap_components as dbc
+
+LOGO = "/assets/logo.png"
+
+# ======================================== #
+# = DEFINING THE NAVBAR AND ITS ELEMENTS = #
+# ======================================== # 
+
+def nav_item(label, href):
+    nav_item_style={
+        'font-size': 20,
+        'display': 'flex',
+        'align-items': 'center',
+        'font-weight': 'normal',
+        'margin-left': 0,
+        'margin-right': 0,
+        'background-color': '#ffffff',
+        'color': '#383838'
+        }
+    return dbc.NavItem(dbc.NavLink(label, href=href, active="exact"), style = nav_item_style)
+
+def drop_down_menu(label, items_url: dict, icon_path):
+    drop_down_menu_style = {
+        "color": "#383838",
+        "font-size": 20,
+        'font-weight': 'normal',
+        "padding-right": "0.5rem",
+        "display": "flex",
+        "align-items": "center",
+        'background-color': '#ffffff'
+    }
+    icon_img = html.Img(
+        src=icon_path, 
+        width='20px', 
+        height='20px', 
+        style={'margin-right': '5px'} # Adiciona um pequeno espaço à direita do ícone
+    )
+
+    dropdown_label_content = html.Span(
+        [icon_img, label],
+        style={
+            'display': 'flex', 
+            'align-items': 'center',
+            'line-height': 'normal' # Ajuda na centralização vertical
+        } 
+    )
+
+    # Cria os itens do menu suspenso (o que cai)
+    children = [
+        dbc.DropdownMenuItem(
+            # Assumindo que 'nav_item' retorna um componente de link (como um 'dbc.NavLink')
+            nav_item(item, items_url[item]), 
+            class_name='ms-2'
+        ) for item in items_url.keys()
+    ]
+
+    return dbc.DropdownMenu(
+        # Passa o Span (que contém o ícone e o texto) como o rótulo
+        label=dropdown_label_content,
+        children=children,
+        toggle_style=drop_down_menu_style,
+        # Você pode querer adicionar um 'className' para estilizar o botão com mais precisão via CSS externo
+    )
+
+# ============ #
+# = NAGATION = #
+# ============ #
+
+REPORTS_PAGES = {
+    'Condições das lavouras': '/reports/condicoes',
+    'Progresso da safra': '/reports/progresso',
+    'Oferta e Demanda - USDA': '/reports/usda',
+    'Exportações (EUA)': '/reports/exportacoes'
+}
+
+SPOT_PAGES = {
+    "Basis": "/mercado-fisico/basis",
+    "Diferencial de base": "/mercado-fisico/basis-diff",
+    "Preços relativo": "/mercado-fisico/rel-prices"
+}
+
+FUT_PAGES = {
+    "Spreads": "/mercado-futuro/spreads",
+    "Ratios": "/mercado-futuro/ratios"
+}
+
+def home_link():
+    return html.Div(children=[
+        html.Img(src="/assets/home_icon.png", width='20px', height='20px'),
+        nav_item("Home", "/")
+    ], style={'display': 'flex', 'align-items': 'center','margin-left': 0, 'margin-right': 0,})
+
+def navbar():
+    return dbc.Navbar(
+        dbc.Container(
+            [
+                # LOGO + 
+                html.A(
+                    dbc.Row(
+                        children=[
+                            html.Div(
+                                    children=[
+                                        # LOGO
+                                        html.Img(src=LOGO, height='100px', className='me-2'),
+                                        #dbc.NavbarBrand("Agrimensor", className='ms-2')
+
+                                        # NAVBAR
+                                        html.Div(
+                                            [
+                                                #nav_item("Home", "/"),
+                                                home_link(),
+                                                drop_down_menu("Relatórios", REPORTS_PAGES, 'assets/reports_icon.png'),
+                                                drop_down_menu("Mercado físico", SPOT_PAGES, 'assets/spot_icon.png'),
+                                                drop_down_menu("Mercado futuro", FUT_PAGES, 'assets/fut_icon.png')
+                                            ],
+                                            style={"display": 'flex'}
+                                        )
+                                    ],
+                                    style={
+                                        "display": 'flex',
+                                        "align-items": "center",
+                                        "justify-content": "center"
+                                    }
+                                )
+                            ]
+                        )
+                    )
+                ]
+            )
+        )
